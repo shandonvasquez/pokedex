88d0fb2d62e6e6363bc336c7050bc208282646ce
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-08-29 23:26:25 -0600

    First changing pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Pagination = ({ cardsPerPage, totalCards, paginate }) => {
+const Pagination = ({ cardsPerPage, totalCards, currentPage, paginate }) => {
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalCards / cardsPerPage); i++) {
@@ -10,23 +10,24 @@ const Pagination = ({ cardsPerPage, totalCards, paginate }) => {
   const onClick = (number, e) => {
     e.preventDefault();
     paginate(number);
-    /* return false; */
   };
 
   return (
     <nav>
       <ul className="pagination">
-        {pageNumbers.map((number) => (
-          <li key={number} className="page-item">
-            <a
-              onClick={(e) => onClick(number, e)}
-              href="!#"
-              className="page-link"
-            >
-              {number}
-            </a>
-          </li>
-        ))}
+        {pageNumbers
+          .filter((number) => number <= 10) // Filtrar las primeras 10 páginas
+          .map((number) => (
+            <li key={number} className={`page-item ${number === currentPage ? "active" : ""}`}>
+              <a
+                onClick={(e) => onClick(number, e)}
+                href="!#"
+                className="page-link"
+              >
+                {number}
+              </a>
+            </li>
+          ))}
       </ul>
     </nav>
   );
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -12,8 +12,8 @@ const PokemonCard = ({ pokemonUrl }) => {
 
   return (
     <Link to={`/pokedex/${pokemonsData.id}`}>
-      <div>
-      <div className={`pokemon-card ${pokemonsData.types?.[0].type.name}`}>
+     <div className="">
+     <div className={`pokemon-card ${pokemonsData.types?.[0].type.name}`}>
         <div className="pokemon-short-info">
           <p>#{("000" + pokemonsData.id).slice(-3)}</p>
           <h1>{pokemonsData.name}</h1>
@@ -26,7 +26,7 @@ const PokemonCard = ({ pokemonUrl }) => {
               </p>
             ))}
           </div>
-        
+         
         </div>
         <div className="pokemon-image">
           <img
@@ -34,12 +34,13 @@ const PokemonCard = ({ pokemonUrl }) => {
             alt={`${pokemonsData.name} sprite`}
           />
         </div>
+     
       </div>
       <p>Hp: {pokemonsData.stats?.[0].base_stat}</p>
-          <p>Attack: {pokemonsData.stats?.[1].base_stat}</p>       
-             <p>Defense: {pokemonsData.stats?.[2].base_stat}</p>
+          <p>Attack: {pokemonsData.stats?.[1].base_stat}</p>
+          <p>Defense: {pokemonsData.stats?.[2].base_stat}</p>
           <p>Velocity: {pokemonsData.stats?.[3].base_stat}</p>
-      </div>
+     </div>
     </Link>
   );
 };
